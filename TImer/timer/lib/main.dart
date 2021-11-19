9dbbe3150996cbf74967257ca49f35140ab3b083
Lovingcats <whehdrms6810@naver.com>
2021-11-19 14:13:08 +0900

    2021-11-19

--- TImer/timer/lib/main.dart
+++ TImer/timer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -20,17 +21,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-bool _iconpress = false;
-
 class TimerPage extends StatefulWidget {
   const TimerPage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   State<TimerPage> createState() => _TimerPageState();
 }
 
 class _TimerPageState extends State<TimerPage> {
-  @override
+  int _timerCount = 0;
+  int _timerCount1 = 0;
+  int _timerCount2 = 0;
+  bool _iconpress = false;
+  late Timer _timer;
+
+  void dispose() {
+    _timer.cancel();
+
+    super.dispose();
+  }
+
+  @override                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -43,16 +55,73 @@ class _TimerPageState extends State<TimerPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
-                height: 320,
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 70,
+                  ),
+                  Text(
+                    "$_timerCount",
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                  Text(
+                    "h : $_timerCount1",
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                  Text(
+                    "m : $_timerCount2",
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                  const Text(
+                    "s",
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 300,
               ),
               IconButton(
                 onPressed: () {
                   setState(() {
                     _iconpress = !_iconpress;
                   });
+                  if (_iconpress) {
+                    _timer = Timer.periodic(const Duration(milliseconds: 1000),
+                        (timer) {
+                      setState(() {
+                        _timerCount2++;
+                        if (_timerCount2 == 60) {
+                          _timerCount2 = 0;
+                          _timerCount1++;
+                          if (_timerCount1 == 60) {
+                            _timerCount1 = 0;
+                            _timerCount++;
+                          }
+                        }
+                      });
+                    });
+                  } else {
+                    _timer.cancel();
+                  }
                 },
                 icon: Icon(_iconpress ? Icons.stop : Icons.play_arrow),
                 iconSize: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _timerCount = 0;
+                    _timerCount1 = 0;
+                    _timerCount2 = 0;
+                    _iconpress = false;
+                    _timer.cancel();
+                  });
+                },
+                style: ElevatedButton.styleFrom(primary: Colors.amber),
+                child: const Text("초기화", style: TextStyle(color: Colors.black)),
               ),
             ],
           ),
