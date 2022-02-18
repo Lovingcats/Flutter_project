20127e15f3e99948edf5a9e60e4be90fa8f45bdd
Lovingcats <whehdrms6810@naver.com>
2022-02-18 17:19:13 +0900

    2022-02-18

---
+++ netflix/netflix/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/widget/bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'netfilx',
+        theme:
+            ThemeData(brightness: Brightness.dark, primaryColor: Colors.black),
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  Container(),
+                  Container(),
+                  Container(),
+                  Container(),
+                ],
+              ),
+              bottomNavigationBar: Bottom(),
+            )));
+  }
+}
---
+++ netflix/netflix/lib/widget/bottom_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Container(
+        height: 50,
+        child: const TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          indicatorColor: Colors.transparent,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.home, size: 18),
+              child: Text(
+                "홈",
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.search, size: 18),
+              child: Text(
+                "검색",
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.save_alt, size: 18),
+              child: Text(
+                "저장한 컨텐츠",
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+            Tab(
+              icon: Icon(Icons.list, size: 18),
+              child: Text(
+                "더보기",
+                style: TextStyle(fontSize: 9),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
