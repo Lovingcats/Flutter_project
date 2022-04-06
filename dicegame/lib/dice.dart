c88e97df50ed845587c2ce10e5717a30b8d78f3a
Lovingcats <whehdrms6810@naver.com>
2022-04-06 20:22:14 +0900

    2022-04-06

--- dicegame/lib/dice.dart
+++ dicegame/lib/dice.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+import 'dart:math';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
@@ -22,8 +10,126 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+  int leftdice = 1;
+  int rightdice = 1;
+  int adddice = 1;
+  int count = -1;
+  bool isstart = false;
+  bool finishpress = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.redAccent,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+          title: const Text("DiceGame"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (isstart)
+                Text('목표 합 : $adddice',
+                    style:
+                        const TextStyle(fontSize: 25, color: Colors.blueAccent))
+              else
+                SizedBox(),
+              if (isstart)
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('images/dice$leftdice.png'),
+                          width: 300,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: Image(
+                          image: AssetImage('images/dice$rightdice.png'),
+                          width: 300,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              if (!isstart)
+                const Text(
+                  "시작버튼을 눌러주세요",
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (!isstart) {
+                        adddice = Random().nextInt(12) + 2;
+                      }
+                      isstart = true;
+                      leftdice = Random().nextInt(6) + 1;
+                      rightdice = Random().nextInt(6) + 1;
+                      count++;
+                      if (adddice == (leftdice + rightdice)) {
+                        correct('$count 번 시도해서 성공하셨습니다!');
+                        count = -1;
+                        isstart = false;
+                      }
+                    });
+                  },
+                  child: Icon(isstart ? Icons.change_circle : Icons.play_arrow),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(75, 50),
+                      primary: Colors.orangeAccent),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                if (isstart)
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isstart = false;
+                        restart();
+                      });
+                    },
+                    child: const Icon(Icons.stop),
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(75, 50),
+                        primary: Colors.orangeAccent),
+                  ),
+              ])
+            ],
+          ),
+        ));
   }
+}
+
+void restart() {
+  Fluttertoast.showToast(
+      msg: "게임을 다시 시작합니다",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.white,
+      textColor: Colors.redAccent,
+      fontSize: 16.0);
+}
+
+void correct(String message) {
+  Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.blue,
+      textColor: Colors.white,
+      fontSize: 16.0);
 }
--- dicegame/lib/main.dart
+++ dicegame/lib/main.dart
@@ -19,13 +19,10 @@ class MyApp extends StatelessWidget {
 }
 
 class Login extends StatelessWidget {
-  
   const Login({Key? key}) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
-    
     TextEditingController dice = TextEditingController();
     TextEditingController password = TextEditingController();
     return Scaffold(
@@ -38,87 +35,106 @@ class Login extends StatelessWidget {
           ],
           leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Padding(padding: EdgeInsets.only(top: 50)),
-              const Center(
-                child: Image(
-                    image: AssetImage('images/profile.jpg'),
-                    width: 170,
-                    height: 150),
-              ),
-              Form(
-                child: Theme(
-                  data: ThemeData(
-                      primaryColor: Colors.teal,
-                      inputDecorationTheme: const InputDecorationTheme(
-                          labelStyle: TextStyle(
-                        color: Colors.teal,
-                        fontSize: 15,
-                      ))),
-                  child: Container(
-                    padding: const EdgeInsets.all(40.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: dice,
-                          decoration: InputDecoration(labelText: 'Enter "dice"'),
-                          keyboardType: TextInputType.emailAddress,
+        body: Builder(builder: (context) {
+          return GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.only(top: 50)),
+                  const Center(
+                    child: Image(
+                        image: AssetImage('images/profile.jpg'),
+                        width: 170,
+                        height: 150),
+                  ),
+                  Form(
+                    child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: const InputDecorationTheme(
+                              labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15,
+                          ))),
+                      child: Container(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              controller: dice,
+                              decoration:
+                                  const InputDecoration(labelText: 'Enter "dice"'),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: password,
+                              decoration:
+                                  const InputDecoration(labelText: 'Enter password'),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            ButtonTheme(
+                                minWidth: 150,
+                                height: 75,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (dice.text == "dice" &&
+                                        password.text == "1234") {
+                                      LoginSucces();
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => const DicePage()),
+                                      );
+                                    } else {
+                                      LoginError();
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.orangeAccent,
+                                  ),
+                                  child: const Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ],
                         ),
-                        TextField(
-                          controller: password,
-                          decoration: InputDecoration(labelText: 'Enter password'),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        ButtonTheme(
-                            minWidth: 150,
-                            height: 75,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                
-                                if (dice.text == "dice" &&
-                                    password.text == "1234") {
-                                  
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => DicePage()),
-                                  );
-                                } else {
-                                  toastMessage();
-                                }
-                              },
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.orangeAccent,
-                              ),
-                              child: const Icon(
-                                Icons.arrow_forward,
-                                color: Colors.white,
-                              ),
-                            )),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-        ));
+            ),
+          );
+        }));
   }
 }
 
-void toastMessage() {
+void LoginError() {
   Fluttertoast.showToast(
       msg: "로그인 정보를 다시 확인해주세요",
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.CENTER,
       timeInSecForIosWeb: 1,
       backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+void LoginSucces() {
+  Fluttertoast.showToast(
+      msg: "로그인 성공!",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.blue,
       textColor: Colors.white,
       fontSize: 16.0);
 }
