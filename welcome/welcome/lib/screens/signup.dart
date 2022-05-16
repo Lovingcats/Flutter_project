57c5d47183fd03a5dd0d973219061e2fef2bcd7f
Lovingcats <whehdrms6810@naver.com>
2022-05-16 22:01:04 +0900

    2022-05-16

--- welcome/welcome/lib/screens/signup.dart
+++ welcome/welcome/lib/screens/signup.dart
@@ -34,29 +34,108 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Center(
-        child: Column(
-          children: [
-            Flexible(
-              flex: 6,
-              fit: FlexFit.tight,
-              child: Container(
-                alignment: Alignment.center,
-                color: const Color(0xffBEE3DB),
+        appBar: null,
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(color: Color(0xffFAF9F9)),
               ),
-            ),
-            Flexible(
-              flex: 4,
-              fit: FlexFit.tight,
-              child: Container(
-                alignment: Alignment.center,
-                color: Color(0xffFAF9F9),
+              Container(
+                height: 380,
+                width: double.infinity,
+                decoration: const BoxDecoration(color: Color(0xffBEE3DB)),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+              const Positioned(
+                child: Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "wELBATO",
+                      style: TextStyle(
+                          fontSize: 43,
+                          color: Color(0xffFAF9F9),
+                          fontFamily: "Gaegu",
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 100, 50, 100),
+                child: Container(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        "회원가입",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Grandstander",
+                            color: Color(0xff89B0AE)),
+                      ),
+                      const Text(
+                        "아래 정보를 입력하세요.",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: "Grandstander",
+                            color: Color(0xff89B0AE)),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          SizedBox(
+                            width: 28,
+                          ),
+                          Text(
+                            "아이디",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontSize: 11,
+                                fontFamily: "GrandStander",
+                                color: Color(0xff555B6E)),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            boxShadow: null,
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                                color: Color(0xffC4D3CC), width: 1.0)),
+                        width: 260,
+                        height: 25,
+                        child: TextField(
+                          textAlign: TextAlign.start,
+                          decoration: InputDecoration(
+                              hintText: '아이디 입력(5~10글자)',
+                              hintStyle: TextStyle(
+                                  fontSize: 10,
+                                  fontFamily: "GrandStander",
+                                  color: Color(0xffC4D3CC),
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      )
+                    ],
+                  ),
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color(0xffFAF9F9),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
