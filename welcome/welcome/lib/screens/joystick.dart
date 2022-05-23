45d46b618ec091f6d5fd296667bc2adf90540d56
Lovingcats <whehdrms6810@naver.com>
2022-05-23 20:10:59 +0900

    2022-05-23

--- welcome/welcome/lib/screens/joystick.dart
+++ welcome/welcome/lib/screens/joystick.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:welcome/screens/instargram_story.dart';
+import 'dart:async';
 
 const step = 10;
 const ballSize = 20.0;
@@ -26,9 +27,25 @@ class JoyStick extends StatefulWidget {
 }
 
 class _JoyStickState extends State<JoyStick> {
+  bool isfind = false;
   double _x = 200;
   double _y = 200;
+  late Timer timer = timer;
   final JoystickMode _joystickMode = JoystickMode.all;
+  int _timerCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -53,13 +70,25 @@ class _JoyStickState extends State<JoyStick> {
                 mode: _joystickMode,
                 listener: (details) {
                   setState(() {
-                    if (_x == 100 && _y == 200) {
+                    if (((_x >= 95) && (_x <= 105)) &&
+                        ((_y >= 195) && (_y <= 205)) &&
+                        isfind == false) {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (_) => StoryPageView()));
-                    } else {
-                      _x = _x + step * details.x;
-                      _y = _y + step * details.y;
+                      isfind = true;
+                      _timerCount = 0;
+                      timer = Timer.periodic(const Duration(milliseconds: 1000),
+                          (timer) {
+                        setState(() {
+                          _timerCount++;
+                          if (_timerCount == 3) {
+                            isfind = false;
+                          }
+                        });
+                      });
                     }
+                    _x = _x + step * details.x;
+                    _y = _y + step * details.y;
                   });
                 },
               ),
