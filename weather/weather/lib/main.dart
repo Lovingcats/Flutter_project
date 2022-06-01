69b2bf2faba79e74a2329c0e112b3b3cb81cdffa
Lovingcats <whehdrms6810@naver.com>
2022-06-01 20:52:24 +0900

    2022-06-01

--- weather/weather/lib/main.dart
+++ weather/weather/lib/main.dart
@@ -48,7 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter1 = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter1++;
     });
   }
 
--- welcome/welcome/lib/screens/camera.dart
+++ welcome/welcome/lib/screens/camera.dart
@@ -66,6 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
         controller = VideoPlayerController.file(File(file.path));
       }
       _controller = controller;
+      // In web, most browsers won't honor a programmatic call to .play
+      // if the video has a sound track (and is not muted).
+      // Mute the video so it auto-plays in web!
+      // This is not needed if the call to .play is the result of user
+      // interaction (clicking on a "play" button, for example).
       const double volume = kIsWeb ? 0.0 : 1.0;
       await controller.setVolume(volume);
       await controller.initialize();
