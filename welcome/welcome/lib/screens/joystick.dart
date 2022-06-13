c40ffcb9d74ad13d8ede4d2ecd6e9b28605598b4
Lovingcats <whehdrms6810@naver.com>
2022-06-14 08:48:43 +0900

    2022-06-14

--- welcome/welcome/lib/screens/joystick.dart
+++ welcome/welcome/lib/screens/joystick.dart
@@ -3,7 +3,7 @@ import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:welcome/screens/instargram_story.dart';
 import 'dart:async';
 
-const step = 3; //한번 이동할때 얼마나 이동할것인가?
+const step = 10; //한번 이동할때 얼마나 이동할것인가?
 const ballSize = 20.0; //이동할 주체의 크기
 double position = 0;
 
@@ -30,25 +30,18 @@ class JoyStick extends StatefulWidget {
 }
 
 class _JoyStickState extends State<JoyStick> {
-  //움직이는 주체의 처음 위치
-  double _x = 100;
-  double _y = 100;
+  //캐릭터가 아닌 맵을 움직임
+  double _x = 2100;
+  double _y = 1000;
   final JoystickMode _joystickMode = JoystickMode.all;
 
   //GlobalKey 선언(위젯의 크기를 구하기 위함)
-  GlobalKey _viewkey = GlobalKey();
+  GlobalKey _mapkey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-
-    //위젯이 그려진 후 _getSize가 실행 됨
-    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-      _getSize();
-    });
   }
-
-  _getSize() {}
 
   @override
   void dispose() {
@@ -57,6 +50,7 @@ class _JoyStickState extends State<JoyStick> {
 
   @override
   Widget build(BuildContext context) {
+    //공의 처음 위치
     double _bx = MediaQuery.of(context).size.width / 2;
     double _by = MediaQuery.of(context).size.height / 2;
     return Scaffold(
@@ -65,9 +59,10 @@ class _JoyStickState extends State<JoyStick> {
         child: Stack(
           children: [
             AnimatedPositioned(
+              //맵의 처음 위치(반응형)
               left: _x * -1,
               top: _y * -1,
-              child: Image.asset("images/mapExample.png"),
+              child: Image.asset("images/map.png"),
               duration: Duration(milliseconds: 200),
             ),
             Positioned(
