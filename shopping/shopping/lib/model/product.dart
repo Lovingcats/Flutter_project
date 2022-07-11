a94d33757c3bb1496373fffa31601d32309e7944
Lovingcats <whehdrms6810@naver.com>
2022-07-11 13:58:17 +0900

    상품 class 생성(id, 상품이름, 상품설명, 가격)

---
+++ shopping/shopping/lib/model/product.dart
@@ -0,0 +1,11 @@
+class product {
+  late final int id;
+  late final String productName;
+  late final String productDescription;
+  late final double price;
+  product(
+      {required this.id,
+      required this.productName,
+      required this.productDescription,
+      required this.price});
+}
