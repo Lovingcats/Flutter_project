d60d7e1eb5940e8d8269267e80e7805a73a3f10c
Lovingcats <whehdrms6810@naver.com>
2022-08-01 04:00:54 +0900

    [CREATE] common.dart

---
+++ welcome/welcome/lib/common/common.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CommonColor {
+  static Color blue = const Color(0xFF9EC3FF);
+  static Color yellow = const Color(0xFFFFEE95);
+  static Color gray = const Color(0xFFB2B2B2);
+}
