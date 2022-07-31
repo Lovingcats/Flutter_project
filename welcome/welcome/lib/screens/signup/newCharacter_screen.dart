35c044b7fad6c772b3bd1d2b5daa2c6723f2bbc9
Lovingcats <whehdrms6810@naver.com>
2022-08-01 04:01:59 +0900

    [CREATE] newCharacter_screen.dart

---
+++ welcome/welcome/lib/screens/signup/newCharacter_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NewCharacter extends StatefulWidget {
+  const NewCharacter({Key? key}) : super(key: key);
+
+  @override
+  State<NewCharacter> createState() => _NewCharacterState();
+}
+
+class _NewCharacterState extends State<NewCharacter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}
