ebbfdbd29a9dc29d7c321e2e2203164954af002f
Lovingcats <whehdrms6810@naver.com>
2022-08-01 04:02:54 +0900

    [CREATE] pw_screen.dart

---
+++ welcome/welcome/lib/screens/signup/pw_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:welcome/common/common.dart';
+import 'package:welcome/screens/signup/name_screen.dart';
+
+class PW extends StatefulWidget {
+  const PW({Key? key}) : super(key: key);
+
+  @override
+  State<PW> createState() => _PWState();
+}
+
+class _PWState extends State<PW> {
+  final _pwdController = TextEditingController();
+  String pwd = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        bottomSheet: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    pwd = _pwdController.text;
+                  });
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => const Name()));
+                },
+                child: Text("다음", style: TextStyle(fontSize: 24.sp)),
+                style: ElevatedButton.styleFrom(
+                    primary: CommonColor.blue, minimumSize: Size(414.w, 59.h)),
+              )),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 12.h, left: 11.w),
+                    child: IconButton(
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(Icons.arrow_back_ios,
+                      size: 22.h,
+                    ),
+                      ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 25.h, left: 20.w, bottom: 27.h, right: 112.w),
+                child: Text(
+                  "사용할 비밀번호를 입력해주세요",
+                  style:
+                      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w, right: 28.w),
+                child: TextField(
+                  controller: _pwdController,
+                  decoration: InputDecoration(
+                    labelText: 'PW',
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: CommonColor.blue, width: 1),
+                    ),
+                    focusedErrorBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 490.h,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
