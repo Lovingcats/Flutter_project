573eb5f4490ad516233fe73b51ff28bbf1c6e5c7
Lovingcats <whehdrms6810@naver.com>
2022-08-01 04:01:11 +0900

    [CREATE] email_screen.dart

---
+++ welcome/welcome/lib/screens/signup/email_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:welcome/common/common.dart';
+import 'package:welcome/screens/signup/emailConfirm_screen.dart';
+
+class Email extends StatefulWidget {
+  const Email({Key? key}) : super(key: key);
+
+  @override
+  State<Email> createState() => _EmailState();
+}
+
+class _EmailState extends State<Email> {
+  final _emailController = TextEditingController();
+  String email = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        bottomSheet: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    email = _emailController.text;
+                  });
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => EmailConfirm()));
+                },
+                child: Text("인증받기", style: TextStyle(fontSize: 24.sp)),
+                style: ElevatedButton.styleFrom(
+                    primary: CommonColor.blue, minimumSize: Size(414.w, 59.h)),
+              )),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 12.h, left: 11.w),
+                    child: IconButton(
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(Icons.arrow_back_ios,
+                      size: 22.h,
+                    ),
+                      ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 25.h, left: 20.w, bottom: 27.h, right: 131.w),
+                child: Text(
+                  "사용할 이메일을 입력해주세요",
+                  style:
+                      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 28.w, right: 28.w),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: CommonColor.blue, width: 1),
+                    ),
+                    focusedErrorBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 490.h,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
