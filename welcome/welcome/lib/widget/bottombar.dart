e3d5773dc1661f34277c3f2ea602b3f810400919
Lovingcats <whehdrms6810@naver.com>
2022-08-05 04:19:42 +0900

    [UPDATE] icon size

--- welcome/welcome/lib/widget/bottombar.dart
+++ welcome/welcome/lib/widget/bottombar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:welcome/common/common.dart';
 
-class Bottom{
+class Bottom {
   static Widget bottomBar() {
     return SizedBox(
       height: 60.h,
@@ -16,31 +16,31 @@ class Bottom{
             Tab(
               icon: Icon(
                 Icons.home,
-                size: 18.h,
+                size: 25.h,
               ),
             ),
             Tab(
               icon: Icon(
                 Icons.groups,
-                size: 18.h,
+                size: 25.h,
               ),
             ),
             Tab(
               icon: Icon(
                 Icons.school,
-                size: 18.h,
+                size: 25.h,
               ),
             ),
             Tab(
               icon: Icon(
                 Icons.chat_bubble,
-                size: 18.h,
+                size: 25.h,
               ),
             ),
             Tab(
               icon: Icon(
                 Icons.account_circle_rounded,
-                size: 18.h,
+                size: 25.h,
               ),
             ),
           ],
