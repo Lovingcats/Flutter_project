d97867ee764c3e67aa7b1c4790ce26538927b52e
Lovingcats <whehdrms6810@naver.com>
2022-08-07 00:57:52 +0900

    [ADD] user_screen ui

--- welcome/welcome/lib/screens/bottombar/user.dart
+++ welcome/welcome/lib/screens/bottombar/user.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:welcome/screens/signup/login.dart';
 
-class User extends StatelessWidget {
+class User extends StatefulWidget {
   const User({Key? key}) : super(key: key);
 
   @override
+  State<User> createState() => _UserState();
+}
+
+class _UserState extends State<User> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text("User Page", style: TextStyle(fontSize: 40.sp),)
-      ],
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 76.h,
+          ),
+          Icon(
+            Icons.circle,
+            color: const Color(0xffD9D9D9),
+            size: 97.h,
+          ),
+          SizedBox(height: 164.h),
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => const Login()));
+              },
+              child: Text(
+                "로그인 하러 가기",
+                style: TextStyle(
+                    fontSize: 20.sp,
+                    decoration: TextDecoration.underline,
+                    color: Colors.black),
+              ))
+        ],
+      ),
     );
   }
 }
