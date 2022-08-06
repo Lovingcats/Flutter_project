a9b71c02ac5a1159af35ba2e7fec1e385d9865cb
Lovingcats <whehdrms6810@naver.com>
2022-08-07 00:56:32 +0900

    [ADD] title ui

--- welcome/welcome/lib/screens/bottombar/chat.dart
+++ welcome/welcome/lib/screens/bottombar/chat.dart
@@ -12,9 +12,41 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text("Chat Page", style: TextStyle(fontSize: 40.sp),)
+        Row(
+          children: [
+            Padding(
+                padding: EdgeInsets.only(
+                  left: 25.w,
+                  top: 26.h,
+                  right: 22.w,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(15.h)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5.r),
+                        spreadRadius: -3.r,
+                        blurRadius: 7.r,
+                        offset: Offset(0.w, 4.h), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  width: 67.w,
+                  height: 67.h,
+                  child: Image.asset("images/chat.png"),
+                )),
+            Text(
+              "대화창",
+              style: TextStyle(
+                fontSize: 30.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
