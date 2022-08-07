16fc8555335b3e3ac512b49472095f18bccd10a6
Lovingcats <whehdrms6810@naver.com>
2022-08-08 02:46:52 +0900

    [CREATE] chatting.dart

---
+++ welcome/welcome/lib/widget/chatting.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:welcome/model/message.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Chatting extends StatefulWidget {
+  const Chatting({Key? key}) : super(key: key);
+
+  @override
+  State<Chatting> createState() => _ChattingState();
+}
+
+class _ChattingState extends State<Chatting> {
+  List<Message> messages = <Message>[];
+  @override
+  void initState() {
+    super.initState();
+
+    messages.add(Message(
+      "바늘이",
+      "아 그런게 좋은거 같아요.",
+    ));
+
+    messages.add(Message(
+      "바늘이",
+      "아 그런게 좋은거 같아요.",
+    ));
+    messages.add(Message(
+      "바늘이",
+      "아 그런게 좋은거 같아요.",
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 20.h),
+            child: ListTile(
+              onTap: () {
+                //push
+              },
+              leading: Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              title: Text(
+                messages[index].name,
+                style: TextStyle(
+                    fontSize: 17.sp,
+                    fontFamily: "Grandstander",
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Padding(
+                padding: EdgeInsets.only(top: 10.h),
+                child: Text(
+                  messages[index].message,
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontFamily: "Grandstander",
+                  ),
+                ),
+              ),
+
+              //dense: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
