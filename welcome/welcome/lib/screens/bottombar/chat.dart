a1d29d6e0f08b7e3c81e1be45b9b70557cfc5190
Lovingcats <whehdrms6810@naver.com>
2022-08-08 02:48:11 +0900

    [ADD] chat UI

--- welcome/welcome/lib/screens/bottombar/chat.dart
+++ welcome/welcome/lib/screens/bottombar/chat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:welcome/widget/chatting.dart';
 
 class Chat extends StatefulWidget {
   const Chat({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
@@ -46,7 +48,79 @@ class _ChatState extends State<Chat> {
               ),
             ),
           ],
-        )
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 25.w, top: 20.h, bottom: 10.h),
+          child: Text("현재 활동 중인 사용자들",
+              style: TextStyle(fontSize: 13.sp, fontWeight: FontWeight.bold)),
+        ),
+        SizedBox(
+          height: 63.h,
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.w),
+            child: ListView(scrollDirection: Axis.horizontal, children: [
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Icon(
+                Icons.circle,
+                size: 63.h,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+            ]),
+          ),
+        ),
+        SizedBox(
+          height: 20.h,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 1.h,
+              width: 377.w,
+              color: const Color(0xffD9D9D9),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 17.h,
+        ),
+        const Chatting()
       ],
     );
   }
