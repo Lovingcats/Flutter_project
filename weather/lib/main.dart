704cc321f395fc146b28c69f75b1249be16d7562
Lovingcats <whehdrms6810@naver.com>
2022-08-10 03:47:38 +0900

    [CREATE] main.dart

---
+++ weather/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:weather/screens/loading.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '날씨앱',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Loading(),
+    );
+  }
+}
