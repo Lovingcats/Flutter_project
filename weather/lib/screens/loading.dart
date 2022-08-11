fae30e9ccac21072ead813ab2548ab4e9bbc32ba
Lovingcats <whehdrms6810@naver.com>
2022-08-12 03:34:37 +0900

    [REFACTORING] api

--- weather/lib/screens/loading.dart
+++ weather/lib/screens/loading.dart
@@ -1,50 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
+import 'package:weather/data/my_location.dart';
+import 'package:weather/data/network.dart';
+import 'package:weather/screens/weather_screen.dart';
+
+const apiKey = '0d0cc1131b44cd6ea0027e60e69dc007';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
 
   @override
-  State<Loading> createState() => _LoadingState();
+  _LoadingState createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  late double latitude3;
+  late double longitude3;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getLoaction();
+    getLocation();
   }
 
-  void getLoaction() async {
-    try {
-      LocationPermission permission = await Geolocator.requestPermission();
+  void getLocation() async {
+    MyLocation myLocation = MyLocation();
+    await myLocation.getMyCurrentLocation();
+    latitude3 = myLocation.latitude2;
+    longitude3 = myLocation.longitude2;
 
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      print(position);
-    } catch (e) {
-      print("인터넷 문제가 발생했습니다");
-    }
+    Network network = Network('https://api.openweathermap.org/data/2.5/weather'
+        '?lat=$latitude3&lon=$longitude3&appid=$apiKey&units=metric&units=metric');
+
+    var weatherData = await network.getJsonData();
+
+    // ignore: use_build_context_synchronously
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return Weather(parseWeatherData: weatherData,);
+    }));
   }
 
-  void getFatch() async {
-    Response response = await get(
-        Uri.parse('https://samples.openweathermap.org/data/2.5/weather?'
-            'q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
-    print(response);
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              getFatch();
-            },
-            child: Text("내 위치 받기")),
+        child: Text(
+          '로딩중..'
+        )
       ),
     );
   }
