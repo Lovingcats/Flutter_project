c77e8fba857199accabc2b4f992eeba974be2d05
Lovingcats <whehdrms6810@naver.com>
2022-08-15 21:58:55 +0900

    [ADD] ui

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.bottom]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return ScreenUtilInit(
       designSize: const Size(400, 700),
       builder: (BuildContext context, Widget? child) => MaterialApp(
@@ -38,54 +37,154 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 100.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 60.w),
-              child: Text(
-                '83°',
-                style: TextStyle(fontSize: 50.sp, fontWeight: FontWeight.w500),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.center,
+              colors: [Color(0xffFC8984), Color(0xffFCB563)])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 100.h,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 60.w),
-              child: Text(
-                'sunny',
-                style: TextStyle(fontSize: 27.sp, fontWeight: FontWeight.w400),
+              Padding(
+                padding: EdgeInsets.only(left: 60.w),
+                child: Text(
+                  '83°',
+                  style:
+                      TextStyle(fontSize: 60.sp, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(right: 20.w),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(
-                    Icons.light_mode,
-                    color: Colors.yellow,
-                    size: 180.h,
-                  )
-                ],
+              Padding(
+                padding: EdgeInsets.only(left: 60.w),
+                child: Text(
+                  'sunny',
+                  style:
+                      TextStyle(fontSize: 37.sp, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 100.h,
-            ),
-                Container(
-                  width: double.infinity,
-                  height: 242.h,
-                  color: Colors.black,
-                  child: Column(
-                    
-                  ),
-                )
-              ],
-            
+              Padding(
+                padding: EdgeInsets.only(right: 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      color: Colors.yellow[200],
+                      size: 180.h,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 52.h,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 55.w, top: 30.h),
+                width: double.infinity,
+                height: 274.8.h,
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Miami",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30.sp,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 1.h,
+                      margin: EdgeInsets.only(top: 10.h, bottom: 10.h),
+                      color: Colors.black,
+                      width: 125.w,
+                    ),
+                    Text(
+                      "July 20",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 40.sp,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.h, bottom: 30.h),
+                      child: Text(
+                        "7:32pm",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.sp,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "CHANCE OF RAIN",
+                              style: TextStyle(
+                                  fontSize: 17.sp,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              height: 3.h,
+                            ),
+                            Text(
+                              "0%",
+                              style: TextStyle(
+                                  fontSize: 21.sp,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 11.w, right: 11.w),
+                          child: Container(
+                            width: 1.w,
+                            height: 35.h,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "HUMIDITY",
+                              style: TextStyle(
+                                  fontSize: 17.sp, color: Colors.black),
+                            ),
+                            SizedBox(
+                              height: 3.h,
+                            ),
+                            Text(
+                              "84%",
+                              style: TextStyle(
+                                  fontSize: 21.sp,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
