f5eb2467af10e6938ef59e40db43d0db14988953
Lovingcats <whehdrms6810@naver.com>
2022-08-28 21:54:10 +0900

    [ADD] 커뮤니티 UI

--- welcome/welcome/lib/widget/post_widget.dart
+++ welcome/welcome/lib/widget/post_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:welcome/model/post.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // ignore: camel_case_types
 class Post_widget extends StatefulWidget {
@@ -22,7 +23,27 @@ class _Post_widgetState extends State<Post_widget> {
       child: ListView.builder(
         itemCount: widget.posts.length,
         itemBuilder: (context, index) {
-          return Padding(padding: EdgeInsets.all(1));
+          return Container(
+            height: 80.w,
+            child: Padding(
+              padding: EdgeInsets.only(left: 27.w, right: 27.w, bottom: 15.h),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                visualDensity: VisualDensity(vertical: -4),
+                leading: Image.asset("images/buttonIcon/fire.png"),
+                tileColor: Colors.white,
+                title: Text(widget.posts[index].title),
+                subtitle: Padding(
+                    padding: EdgeInsets.only(left: 200.w),
+                    child: Text(
+                      "조회수 ${widget.posts[index].views}",
+                      style: TextStyle(fontSize: 12.sp),
+                    )),
+              ),
+            ),
+          );
         },
       ),
     );
