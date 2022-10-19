6e6cac84450706fec1224426c7b21da4ca23c42a
Lovingcats <whehdrms6810@naver.com>
2022-10-19 20:01:23 +0900

    [ADD] getrequest

--- welcome/welcome/lib/screens/signup/emailConfirm_screen.dart
+++ welcome/welcome/lib/screens/signup/emailConfirm_screen.dart
@@ -1,16 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:welcome/common/common.dart';
 import 'package:welcome/screens/signup/status_screen.dart';
+import 'package:http/http.dart' as http;
 
-class EmailConfirm extends StatelessWidget {
-  final _confirmController = TextEditingController(); //textfield를 위한 컨트롤러
+class EmailConfirm extends StatefulWidget {
+  const EmailConfirm({Key? key}) : super(key: key);
+
+  @override
+  State<EmailConfirm> createState() => _EmailConfirmState();
+}
+
+class _EmailConfirmState extends State<EmailConfirm> {
+  final _confirmController = TextEditingController();
+  //textfield를 위한 컨트롤러
   String confirm = '';
+
+  void getrequest(String confirm1) async {
+    print("실행됨");
+    String url = 'http://13.125.225.199:8003/login/checkCode?code=$confirm1';
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var parsingData = jsonDecode(utf8.decode(response.bodyBytes));
+      print(parsingData);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const Status()));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
@@ -20,9 +44,10 @@ class EmailConfirm extends StatelessWidget {
               padding: EdgeInsets.only(
                   bottom: MediaQuery.of(context).viewInsets.bottom),
               child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const Status()));
+                onPressed: () {setState(() {
+          confirm = _confirmController.text;
+        });
+                  getrequest(confirm);
                 },
                 child: Text("다음", style: TextStyle(fontSize: 24.sp)),
                 style: ElevatedButton.styleFrom(
@@ -79,7 +104,6 @@ class EmailConfirm extends StatelessWidget {
               SizedBox(
                 height: 490.h,
               ),
-              
             ],
           ),
         ),
