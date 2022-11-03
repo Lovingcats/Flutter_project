86c746bcdf3af8149264c787f23285849dd6c12f
Lovingcats <whehdrms6810@naver.com>
2022-11-03 21:50:26 +0900

    [ADD] background

--- welcome/welcome/lib/common/common.dart
+++ welcome/welcome/lib/common/common.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-class CommonColor { //색을 정리해둔 코드
+class CommonColor {
+  //색을 정리해둔 코드
   static Color blue = const Color(0xFF9EC3FF);
   static Color yellow = const Color(0xFFFFEE95);
   static Color gray = const Color(0xFFB2B2B2);
+  static Color background = const Color(0xFFEFF5FF);
 }
