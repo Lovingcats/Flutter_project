218d29402880151d5550ce97cb6adf91216c1ea7
Lovingcats <whehdrms6810@naver.com>
2022-11-13 21:45:02 +0900

    [CREATE] buttons.dart

---
+++ welcome/welcome/lib/widget/buttons.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:welcome/common/common.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+
+class Buttons extends StatefulWidget {
+  final String url;
+  final String name;
+  // ignore: prefer_typing_uninitialized_variables
+  final where;
+  const Buttons({Key? key, required this.url, required this.name, this.where})
+      : super(key: key);
+
+  @override
+  State<Buttons> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends State<Buttons> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => widget.where));
+          },
+          child: Image.asset(widget.url),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            minimumSize: Size(67.w, 67.h),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.r)),
+          ),
+        ),
+        SizedBox(
+          height: 8.h,
+        ),
+        Text(
+          widget.name,
+          style: TextStyle(
+              fontSize: 13.sp,
+              color: Colors.black,
+              fontWeight: FontWeight.w500),
+        )
+      ],
+    );
+  }
+}
