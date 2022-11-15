75b020fae73cd549a253fd07d2af83bef662cb1a
Lovingcats <whehdrms6810@naver.com>
2022-11-15 10:12:15 +0900

    [UPDATE] UI

--- welcome/welcome/lib/screens/bottombar/home.dart
+++ welcome/welcome/lib/screens/bottombar/home.dart
@@ -1,8 +1,12 @@
-import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:welcome/common/common.dart';
+import 'package:welcome/screens/bottombar/metawebview.dart';
+import 'package:welcome/screens/eat.dart';
+import 'package:welcome/screens/schedule.dart';
+import 'package:welcome/screens/time.dart';
+import 'package:welcome/widget/Buttons.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,102 +19,102 @@ class _HomeState extends State<Home> {
   int count = 1; //ImageSlideShow를 위한 변수 count
   int pageIndex = 2; // List<Widget> container의 배열을 이용하기 위한 변수 count
 
-  List<Widget> containers = [
-    // container들의 list, 익명커뮤, 뜨근 조언등등을 list로 정리
-    GestureDetector(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(10.h))),
-        margin: EdgeInsets.only(top: 22.h, bottom: 22.h),
-        width: 300.w,
-        height: 265.h,
-        child: Padding(
-          padding: EdgeInsets.all(23.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "뜨끈 조언",
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
-              ),
-              Text(
-                "서로 조언을 주고 받을 수 있는\n커뮤니티",
-                style: TextStyle(fontSize: 18.sp),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-    GestureDetector(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(10.h))),
-        margin: EdgeInsets.fromLTRB(61.w, 22.h, 61.w, 22.h),
-        width: 292.w,
-        height: 265.h,
-        child: Padding(
-          padding: EdgeInsets.only(top: 23.h, left: 23.w, right: 23.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "일반 커뮤니티",
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
-              ),
-              Text(
-                "사람들과\n소통할 수 있는 커뮤니티",
-                style: TextStyle(fontSize: 18.sp),
-              ),
-              SizedBox(
-                height: 55.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset(
-                    "images/community.png",
-                    scale: 1.3,
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    ),
-    GestureDetector(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(10.h))),
-        margin: EdgeInsets.fromLTRB(61.w, 22.h, 61.w, 22.h),
-        width: 292.w,
-        height: 265.h,
-        child: Padding(
-          padding: EdgeInsets.all(23.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "익명 커뮤니티",
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
-              ),
-              Text(
-                "사람들과 익명으로\n소통할 수 있는 공간",
-                style: TextStyle(fontSize: 18.sp),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  ];
+  // List<Widget> containers = [
+  //   // container들의 list, 익명커뮤, 뜨근 조언등등을 list로 정리
+  //   GestureDetector(
+  //     onTap: () {},
+  //     child: Container(
+  //       decoration: BoxDecoration(
+  //           color: Colors.white,
+  //           borderRadius: BorderRadius.all(Radius.circular(10.h))),
+  //       margin: EdgeInsets.only(top: 22.h, bottom: 22.h),
+  //       width: 300.w,
+  //       height: 265.h,
+  //       child: Padding(
+  //         padding: EdgeInsets.all(23.h),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Text(
+  //               "뜨끈 조언",
+  //               style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
+  //             ),
+  //             Text(
+  //               "서로 조언을 주고 받을 수 있는\n커뮤니티",
+  //               style: TextStyle(fontSize: 18.sp),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   ),
+  //   GestureDetector(
+  //     onTap: () {},
+  //     child: Container(
+  //       decoration: BoxDecoration(
+  //           color: Colors.white,
+  //           borderRadius: BorderRadius.all(Radius.circular(10.h))),
+  //       margin: EdgeInsets.fromLTRB(61.w, 22.h, 61.w, 22.h),
+  //       width: 292.w,
+  //       height: 265.h,
+  //       child: Padding(
+  //         padding: EdgeInsets.only(top: 23.h, left: 23.w, right: 23.w),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Text(
+  //               "일반 커뮤니티",
+  //               style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
+  //             ),
+  //             Text(
+  //               "사람들과\n소통할 수 있는 커뮤니티",
+  //               style: TextStyle(fontSize: 18.sp),
+  //             ),
+  //             SizedBox(
+  //               height: 55.h,
+  //             ),
+  //             Row(
+  //               mainAxisAlignment: MainAxisAlignment.end,
+  //               children: [
+  //                 Image.asset(
+  //                   "images/community.png",
+  //                   scale: 1.3,
+  //                 )
+  //               ],
+  //             )
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   ),
+  //   GestureDetector(
+  //     onTap: () {},
+  //     child: Container(
+  //       decoration: BoxDecoration(
+  //           color: Colors.white,
+  //           borderRadius: BorderRadius.all(Radius.circular(10.h))),
+  //       margin: EdgeInsets.fromLTRB(61.w, 22.h, 61.w, 22.h),
+  //       width: 292.w,
+  //       height: 265.h,
+  //       child: Padding(
+  //         padding: EdgeInsets.all(23.h),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             Text(
+  //               "익명 커뮤니티",
+  //               style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30.sp),
+  //             ),
+  //             Text(
+  //               "사람들과 익명으로\n소통할 수 있는 공간",
+  //               style: TextStyle(fontSize: 18.sp),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   ),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -195,119 +199,33 @@ class _HomeState extends State<Home> {
             ],
           ),
           Padding(
-            padding: EdgeInsets.only(
-                top: 13.h, left: 28.w, right: 28.w, bottom: 8.h),
+            padding: EdgeInsets.only(left: 23.w, right: 23.w, top: 21.h),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Image.asset("images/buttonIcon/eat.png"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        minimumSize: Size(67.w, 67.h),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(23.h)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                    Text(
-                      "급식",
-                      style: TextStyle(
-                          fontSize: 12.sp, fontWeight: FontWeight.w500),
-                    ),
-                  ],
+              children: const [
+                Buttons(
+                  url: "images/buttonIcon/eat.png",
+                  name: "급식",
+                  where: Eat(),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Image.asset("images/buttonIcon/time.png"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        minimumSize: Size(67.w, 67.h),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(23.h)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                    Text(
-                      "시간표",
-                      style: TextStyle(
-                          fontSize: 12.sp, fontWeight: FontWeight.w500),
-                    ),
-                  ],
+                Buttons(
+                  url: "images/buttonIcon/time.png",
+                  name: "시간표",
+                  where: Time(),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Image.asset("images/buttonIcon/calender.png"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        minimumSize: Size(67.w, 67.h),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(23.h)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                    Text(
-                      "학사일정",
-                      style: TextStyle(
-                          fontSize: 12.sp, fontWeight: FontWeight.w500),
-                    ),
-                  ],
+                Buttons(
+                  url: "images/buttonIcon/calender.png",
+                  name: "학사일정",
+                  where: Schedule(),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Image.asset("images/buttonIcon/meta.png"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        minimumSize: Size(67.w, 67.h),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(23.h)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                    Text(
-                      "메타버스",
-                      style: TextStyle(
-                          fontSize: 12.sp, fontWeight: FontWeight.w500),
-                    ),
-                  ],
+                Buttons(
+                  url: "images/buttonIcon/meta.png",
+                  name: "VR",
+                  where: MetaWebview(),
                 ),
               ],
             ),
           ),
-          Container(
-            color: CommonColor.background,
-            width: double.infinity,
-            height: 311.h,
-            child: Swiper(
-              itemBuilder: (BuildContext context, int index) {
-                return containers[0];
-              },
-              itemCount: containers.length,
-              viewportFraction: 0.8,
-              scale: 0.9,
-            ),
-          )
         ]));
   }
 }
