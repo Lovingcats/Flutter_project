857335e82b234123c7fa109d0b5edf2896ebbd6d
Lovingcats <whehdrms6810@naver.com>
2022-11-15 10:18:51 +0900

    [CREATE] postprovider.dart

---
+++ welcome/welcome/lib/provider/postprovider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:welcome/model/post.dart';
+
+class PostProviderData extends ChangeNotifier {
+  late List<Post> _normalPosts = <Post>[];
+  late List<Post> _blindPosts = <Post>[];
+  late List<Post> _advicePosts = <Post>[];
+  List<Post> get normalPosts => _normalPosts;
+  List<Post> get blindPosts => _blindPosts;
+  List<Post> get advicePosts => _advicePosts;
+
+  void inputNormalData(String title, int views, String contact, int heart,
+      String userName) {
+    normalPosts.add(Post(title, views, contact, heart, userName));
+  }
+
+  void inputBlindData(String title, int views, String contact, int heart,
+      String userName) {
+    blindPosts.add(Post(title, views, contact, heart, userName));
+  }
+
+  void inputAdviceData(String title, int views, String contact, int heart,
+      String userName) {
+    advicePosts.add(Post(title, views, contact, heart, userName));
+  }
+}
