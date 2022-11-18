db82d239112d20bbee3823736bff0d88616cbbaa
Lovingcats <whehdrms6810@naver.com>
2022-11-18 23:42:44 +0900

    [CREATE] userproivder.dart

---
+++ welcome/welcome/lib/provider/userprovider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UserData extends ChangeNotifier {
+  String _accessToekn = '';
+  String _userName = '';
+  String _userId = '';
+
+  String get accessToken => _accessToekn;
+  String get userName => _userName;
+  String get userId => _userId;
+
+  void inputAccessToken(String accessToken) {
+    _accessToekn = accessToken;
+    notifyListeners();
+  }
+
+  void inputDatas(String userName1, String userId1) {
+    _userName = userName1;
+    _userId = userId1;
+    notifyListeners();
+  }
+}
