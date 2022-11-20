c650a281aaad859703f56af66b89fdf685e0878c
Lovingcats <whehdrms6810@naver.com>
2022-11-20 22:31:46 +0900

    [ADD] provider

--- welcome/welcome/lib/screens/bottombar/user.dart
+++ welcome/welcome/lib/screens/bottombar/user.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:welcome/main.dart';
+import 'package:welcome/provider/userprovider.dart';
 
 class User extends StatefulWidget {
   const User({Key? key}) : super(key: key);
@@ -12,6 +16,7 @@ class User extends StatefulWidget {
 class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
+    var userData = Provider.of<UserData>(context);
     return Center(
       child: Column(
         children: [
@@ -23,19 +28,24 @@ class _UserState extends State<User> {
             color: const Color(0xffD9D9D9),
             size: 97.h,
           ),
-          SizedBox(height: 164.h),
-          TextButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => const Login()));
-              },
-              child: Text(
-                "로그인 하러 가기",
-                style: TextStyle(
-                    fontSize: 20.sp,
-                    decoration: TextDecoration.underline,
-                    color: Colors.black),
-              ))
+          Padding(
+            padding: EdgeInsets.only(top: 10.h, bottom: 130.h),
+            child: Text(
+              userData.userName + "님",
+              style: TextStyle(
+                  fontSize: 20.sp,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => const Login()));
+            },
+            child: const Text("로그아웃"),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+          ),
         ],
       ),
     );
