2c7ca7148ea586b92bbd119385a70b8e6d2c0967
Lovingcats <whehdrms6810@naver.com>
2022-11-28 19:55:20 +0900

    [UPDATE] userdata

--- welcome/welcome/lib/screens/bottombar/user.dart
+++ welcome/welcome/lib/screens/bottombar/user.dart
@@ -17,37 +17,58 @@ class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
     var userData = Provider.of<UserData>(context);
-    return Center(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 76.h,
-          ),
-          Icon(
-            Icons.account_circle,
-            color: const Color(0xffD9D9D9),
-            size: 97.h,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.h, bottom: 130.h),
-            child: Text(
-              userData.userName + "님",
-              style: TextStyle(
-                  fontSize: 20.sp,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500),
+    return userData.login
+        ? Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 76.h,
+                ),
+                Icon(
+                  Icons.account_circle,
+                  color: const Color(0xffD9D9D9),
+                  size: 97.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.h, bottom: 130.h),
+                  child: Text(
+                    userData.userName + "님",
+                    style: TextStyle(
+                        fontSize: 20.sp,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      userData.loginChange(false);
+                    });
+                  },
+                  child: const Text("로그아웃"),
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                ),
+              ],
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => const Login()));
-            },
-            child: const Text("로그아웃"),
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-          ),
-        ],
-      ),
-    );
+          )
+        : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => const Login()));
+                    },
+                    child: Text(
+                      "로그인 하러 가기",
+                      style: TextStyle(
+                          fontSize: 17.sp, fontWeight: FontWeight.w500),
+                    ))
+              ],
+            ),
+          );
   }
 }
