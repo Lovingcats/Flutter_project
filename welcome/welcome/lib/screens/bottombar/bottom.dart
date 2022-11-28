b1256fa77c9a21a56103f1137570c65aaa404f8f
Lovingcats <whehdrms6810@naver.com>
2022-11-28 19:54:31 +0900

    [UPDATE] bottombar length

--- welcome/welcome/lib/screens/bottombar/bottom.dart
+++ welcome/welcome/lib/screens/bottombar/bottom.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:welcome/screens/bottombar/community.dart';
 import 'package:welcome/screens/bottombar/home.dart';
-import 'package:welcome/screens/bottombar/meta.dart';
 import 'package:welcome/screens/bottombar/user.dart';
 import 'package:welcome/widget/bottombar.dart';
 // import 'package:http/http.dart' as http;
@@ -18,13 +17,13 @@ class _BottombarState extends State<Bottombar> {
   Widget build(BuildContext context) {
     return DefaultTabController(
         //바텀바 코드
-        length: 4,
+        length: 3,
         child: Scaffold(
             body: const TabBarView(
               physics: NeverScrollableScrollPhysics(),
               children: [
                 Home(),
-                Meta(),
+                
                 Community(),
                 User()
               ], //각각 home, community, meta, chat, user로 이동할수 있게 함
