754891f5e2eadf6c9b26bf37b5af9bef485c2723
Lovingcats <whehdrms6810@naver.com>
2022-11-28 19:53:28 +0900

    [ADD] userData provider

--- welcome/welcome/lib/provider/userprovider.dart
+++ welcome/welcome/lib/provider/userprovider.dart
@@ -4,10 +4,17 @@ class UserData extends ChangeNotifier {
   String _accessToekn = '';
   String _userName = '';
   String _userId = '';
+  bool _login = false;
 
   String get accessToken => _accessToekn;
   String get userName => _userName;
   String get userId => _userId;
+  bool get login => _login;
+
+  void loginChange(bool bools) {
+    _login = bools;
+    notifyListeners();
+  }
 
   void inputAccessToken(String accessToken) {
     _accessToekn = accessToken;
@@ -16,7 +23,7 @@ class UserData extends ChangeNotifier {
 
   void inputDatas(String userName1, String userId1) {
     _userName = userName1;
-     _userId = userId1;
+    _userId = userId1;
     notifyListeners();
   }
 }
