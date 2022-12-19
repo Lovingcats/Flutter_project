61c9f4d311a2dd61b519b620f58a42d14352d336
Lovingcats <whehdrms6810@naver.com>
2022-12-20 08:11:33 +0900

    [ADD] import

--- welcome/welcome/lib/screens/signup/id_pw/completeid.dart
+++ welcome/welcome/lib/screens/signup/id_pw/completeid.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:welcome/common/common.dart';
 import 'package:welcome/main.dart';
+import 'package:welcome/screens/login.dart';
 import 'package:welcome/screens/signup/id_pw/idtodart.dart';
 
 class CompleteId extends StatefulWidget {
