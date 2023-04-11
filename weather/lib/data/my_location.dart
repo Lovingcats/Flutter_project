0312e942cd6f1abf151d5f18dbd3d08c73b19676
Lovingcats <whehdrms6810@naver.com>
2023-04-11 13:49:49 +0900

    [UPDATE] 변수 late 말고 초기화

--- weather/lib/data/my_location.dart
+++ weather/lib/data/my_location.dart
@@ -1,18 +1,18 @@
 import 'package:geolocator/geolocator.dart';
 
-class MyLocation{
-  late double latitude2;
-  late double longitude2;
+class MyLocation {
+  late double latitude2 = 0.0;
+  late double longitude2 = 0.0;
 
-  Future<void> getMyCurrentLocation() async{
+  Future<void> getMyCurrentLocation() async {
     try {
-      Position position =  await Geolocator.
-      getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       latitude2 = position.latitude;
       longitude2 = position.longitude;
       print(latitude2);
       print(longitude2);
-    }catch(e){
+    } catch (e) {
       print('There was a problem with the internet connection.');
     }
   }
