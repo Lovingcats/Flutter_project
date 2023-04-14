27faf18d551a863e5422008f07019b44d7626f36
Lovingcats <whehdrms6810@naver.com>
2023-04-14 10:17:14 +0900

    [UPDATE] Loading() -> Weather()

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Loading(),
+        home: const Weather(),
       ),
     );
   }
